256af9f579e829d1188616166460889caa2a8b1b
GwynElly <l0rdgwyn@yandex.ru>
2021-05-06 19:24:13 +0300

    New2021-06-05

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
-var x1, x2, x3, x4; 
-var y1, y2;
 var A1, A2;
-var z0, z1, z2, z3, z4;
+var z0, z1, z2, z3, z4; 
 
 z0 = 20 + Math.floor(Math.random() * (100 - 20)); //first 
 z1 = Math.floor(Math.random() * 100 + 1); //for +
 z2 = Math.floor(Math.random() * z0 - 1 ); //for -
 z3 = Math.floor(Math.random() * 10);  //for *
-console.log('first', z0, '\n+', z1, '\n-', z2, '\n*', z3/*, '\n/', z4*/);
-x1 = 1; //+
-x2 = 2; //-
-x3 = 3; //*
-x4 = 4; //"/"
-var arr1 = [x1, x2, x3, x4];
-var arrRand = Math.floor(Math.random() * 4);
-console.log('rand arr -', arr1[arrRand]);
 
-console.log('Z0_f', z0);
-console.log('-----------------------');
-if (arrRand == 3) {
+
+console.log('F', z0, '\n+', z1, '\n-', z2, '\n*', z3);
+
+
+var arr1 = [1, 2, 3, 4];
+var arrRand = Math.floor(Math.random() * 4);
+console.log('randomArr = ', arr1[arrRand]);
+
+
+if (arrRand == 3) { 
 for (var z4 = (2 + Math.floor(Math.random() * (7 - 2))); (z0 % z4 !== 0); z4++) {
- 	console.log('AHAHAHHAHAAH DAI IST ARBEITE', z4);
+ 	//console.log('/ +1', z4);
 } 
-console.log('mmononoon', z4);
-console.log('-----------------------');
+console.log('Delim na', z4);
+console.log('<----------------------->');
 }
-switch(arr1[arrRand]) {
+
+
+let NumFirstHtml = document.getElementById('first');
+let NumSecondHtml = document.getElementById('second');
+let ZnakHtml = document.getElementById('znak');
+NumFirstHtml.value = z0;
+NumFirstHtml.textContent = z0;
+
+switch(arr1[arrRand]) { 
 	case 1:
  				console.log('PLUS (+)');
  				var A1 = z0 + z1; 
- 				console.log(A1);
+ 				NumSecondHtml.textContent = z1;
+				ZnakHtml.textContent = "+";
+ 				console.log('answer = ', A1, '\n', NumSecondHtml.textContent, '\n', ZnakHtml.textContent);
 			break;
 	case 2:
 			 		console.log('MIN (-)');
 			 	 	var A1 = z0 - z2; 
-			 		console.log(A1);
+					NumSecondHtml.textContent = z2;
+					ZnakHtml.textContent = "-";
+			 		console.log('answer = ', A1, '\n', NumSecondHtml.textContent, '\n', ZnakHtml.textContent);
 			break;
 	case 3:
 			 			console.log('UMN (*)');
 			 	 		var A1 = z0 * z3; 
-			 			console.log(A1);
+			 	 		NumSecondHtml.textContent = z3;
+						ZnakHtml.textContent = "*";
+			 			console.log('answer = ', A1, '\n', NumSecondHtml.textContent, '\n', ZnakHtml.textContent);
 			break;
 	case 4:
 			  		console.log('DELEN (/)');
 			  	 	var A1 = z0 / z4; 
-			 		console.log(A1.toFixed(2));	
+			  	 	NumSecondHtml.textContent = z4;
+					ZnakHtml.textContent = "/";
+			 		console.log('answer = ', A1.toFixed(2), '\n', NumSecondHtml.textContent, '\n', ZnakHtml.textContent);	
 			break;
 }
 
-function proverka() {
+function CheckParagr() {
+	let answerParagraph = document.getElementById('answerPharagr');
 	let answerrr = document.getElementById('AnswerId');
-	var inputAnswerValue = answerrr.value
-	console.log(inputAnswerValue);
-		if (inputAnswerValue == A1) {
-			console.log('yes');
-		}
-		else {
-			console.log('no');
-		}
+			if (answerrr.value == A1) {
+				answerParagraph.textContent = 'Recht!';
+			}
+			else {
+				answerParagraph.textContent = 'Falsch!';
+			}
 }
-
-
-//------------------------------------------------------------
-// Разобраться с вычитаением делением и умножением  - ✔
-// сделать чтобы можно было вводить инпут - ✔
-// Забирать валуе из инпута - ✔
-// Добавить проверку инпута - 
-// Добавить чтобы можно было выбирать действие - 
-// Сделать нормальный дизайн - 
-//------------------------------------------------------------
